df6039b469367dd76345e96a90f0af12ed932a7c
Katie McBride <archmcbride@hotmail.com>
2017-08-01 15:25:27 -0600

    revamped based on class information

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-<html>
+$(document).ready(function(){ //when the document is ready, then run this code
+	$('#myNameP').on('click', function() {
+		$('#myNameP').addClass('hidden')
+		$('#myNameInput').removeClass('hidden')
+		$('#myNameInput').trigger('focus')
+		$('#myNameInput').val($('#MyNameP').text())
+	
+	})
+
+	$('myNameInput').on('blur', function(){ 
+		$('#myNameP').text($('#myNameInput').val())
+		$('#myNameP').removeClass('hidden')
+		$('#myNameInput').addClass('hidden')
+	})
+	///////////////////Section 2/////////////////
+	$('#bioP').on('click', function(){
+		$('#bioTextArea').val($('#bioP').text())
+		$('#bioP').addClass('hidden')
+		$('#bioTextArea').removeClass('hidden')
+		$('#bioTextArea').trigger('focus')
+		
+		
+	})
+
+	$('#bioTextArea').on('blur', function(){
+		$('#bioP').text($('#bioTextArea').val())
+		$('#bioP').removeClass('hidden')
+		$('#bioTextArea').addClass('hidden')
+
+	})
+})
